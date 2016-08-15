88b2893ef50925523de9cf1b243d7475995dca2b
annachiu7 <qujiani7879@126.com>
2016-08-15 16:30:51 +0200

    camera zum struct umgeschrieben, definematerial(sdf) updated

--- framework/camera.hpp
+++ framework/camera.hpp
@@ -5,13 +5,11 @@
 #include <string>
 #include "ray.hpp"
 
-class Camera
+struct Camera
 {
-public:
 	Camera();
 	Camera(std::string const& name, glm::vec3 const& pos, double fov_x);
 	~Camera();
-private:
 	std::string name_;
 	glm::vec3 pos_;
 	double fov_x_;
