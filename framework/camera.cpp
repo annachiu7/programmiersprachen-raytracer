4fd5a866db95e2e8fbc82aff3bdc7a2ac61ad464
annachiu7 <qujiani7879@126.com>
2016-08-17 15:33:42 +0200

    das einfache kameramodell verbessert und funktioniert!

--- framework/camera.cpp
+++ framework/camera.cpp
@@ -1,4 +1,5 @@
 #include "camera.hpp"
+#include <cmath>
 
 Camera::Camera():
 name_{""},
@@ -12,7 +13,8 @@ fov_x_{fov_x},
 origin_{origin}
 {}
 
-Camera::Camera(std::string const& name, double fov_x, glm::vec3 const& origin, glm::vec3 const& dir, glm::vec3 const& up):
+Camera::Camera(std::string const& name, double fov_x, glm::vec3 const& origin, 
+			   glm::vec3 const& dir, glm::vec3 const& up):
 name_{name},
 fov_x_{fov_x},
 origin_{origin},
@@ -22,9 +24,13 @@ up_{up}
 
 Ray Camera::calc_eye_ray(int x, int y, int height, int width)
 {
-  //glm::vec3 origin{0,0,0};
-  glm::vec3 direction{float(x)*2.0/float(width) -1.0,
-                      float(y)*2.0/float(height) -1.0,-1.0};
-  Ray ray{origin_, direction};
-  return ray;
+	// -------- das einfache Kameramodell -------- //
+	// ----- Die Breite der Bildebene ist 1 ------ //
+	// Blickrichtung ist die negative z-Richtung!
+	// Die Position der Camera kann sich aber irgendwo befinden
+  	glm::vec3 direction{float(x)*1.0/float(width) -0.5,
+                        float(y)*1.0/float(height) -0.5, 
+                        -1.0*(0.5/tan(fov_x_/2))}; // distance = 0.5 / tan(winkel/2)
+  	Ray ray{origin_, direction};
+  	return ray;
 }
