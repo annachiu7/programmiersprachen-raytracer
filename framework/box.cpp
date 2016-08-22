81504fb1f1f8975589c74fac057b97cae1aa5873
piza2416 <fabian.platz@uni-weimar.de>
2016-08-22 16:24:23 +0200

    normalenerechnung in intersect box integriert

--- framework/box.cpp
+++ framework/box.cpp
@@ -41,7 +41,7 @@ std::ostream& Box::print(std::ostream& os) const
 OptiHit Box::intersect(Ray const& ray) const
 {
 //{{{
-#if 1
+
   OptiHit hit{this};
   float tmin = -INFINITY, tmax = INFINITY;
 
@@ -72,76 +72,34 @@ OptiHit Box::intersect(Ray const& ray) const
     hit.surface_pt = this->calc_surface_pt(ray, hit.distance);
     hit.surface_pt = ray.origin_ + hit.distance * ray.direction_;
     hit.hit = true;    
+    auto surface_pt = hit.surface_pt; 
+    if(surface_pt.x == Approx(min_.x))
+    {
+      hit.n = glm::vec3{-1.0,0.0,0.0};
+    }
+    if(surface_pt.y == Approx(min_.y))
+    {
+      hit.n = glm::vec3{0.0,-1.0,0.0};
+    }
+    if(surface_pt.z == Approx(min_.z))
+    {
+      hit.n = glm::vec3{0.0,0.0,-1.0};
+    }
+    if(surface_pt.x == Approx(max_.x))
+    {
+      hit.n = glm::vec3{1.0,0.0,0.0};
+    }
+    if(surface_pt.y == Approx(max_.y))
+    {
+      hit.n = glm::vec3{0.0,1.0,0.0};
+    }
+    if(surface_pt.z == Approx(max_.z))
+    {
+      hit.n = glm::vec3{0.0,0.0,1.0};
+    }
   }
   return hit;
 }
-#endif
-//}}}
-#if 0
-  OptiHit hit{this};
-	float tnear,tfar;
-	//distance = -1;
-
-	if (ray.direction_.x != 0.0)
-	{
-		float t1 = (min_.x - ray.origin_.x) / ray.direction_.x;
-		float t2 = (max_.x - ray.origin_.x) / ray.direction_.x;
-		tfar = std::max(t1,t2);
-		tnear = std::min(t1,t2);
-		
-	}
-	else 
-	{
-		if(min_.x > ray.origin_.x || max_.x < ray.origin_.x) {
-      return OptiHit{};
-    }
-	}
-
-	if (ray.direction_.y != 0.0)
-	{
-		float t1 = (min_.y - ray.origin_.y) / ray.direction_.y;
-		float t2 = (max_.y - ray.origin_.y) / ray.direction_.y;
-		float tnear = std::max(tnear, std::min(t1,t2));
-		float tfar = std::min(tfar, std::max(t1,t2));
-		if (tnear > tfar)
-		{
-      return OptiHit{};
-		}
-	}
-	else 
-	{
-		if(min_.y > ray.origin_.y || max_.y < ray.origin_.y) {
-      return OptiHit{};
-    }
-	}
-
-	if (ray.direction_.z != 0.0)
-	{
-		float t1 = (min_.z - ray.origin_.z) / ray.direction_.z;
-		float t2 = (max_.z - ray.origin_.z) / ray.direction_.z;
-		tnear = std::max(tnear, std::min(t1,t2));
-		tfar = std::min(tfar, std::max(t1,t2));
-		if (tnear > tfar)
-		{
-      return OptiHit{};
-		}
-	}
-	else 
-	{
-		if(min_.z > ray.origin_.z || max_.z < ray.origin_.z) {
-      return OptiHit{};
-    }
-	}
-
-	hit.distance = tnear*sqrt(ray.direction_.x*ray.direction_.x +
-					      ray.direction_.y*ray.direction_.y +
-					      ray.direction_.z*ray.direction_.z);
-  hit.closest_shape = this;
-  hit.hit = true;
-  hit.surface_pt = this->calc_surface_pt(ray,hit.distance);
-	return hit;
-}
-#endif
 
 //aufgabe5.10
 
