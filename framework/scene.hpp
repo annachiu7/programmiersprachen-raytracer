f85bb9caeb03b1ec8dead22e7e615e5071e83d70
piza2416 <fabian.platz@uni-weimar.de>
2016-08-24 15:27:55 +0200

    funktion zur berechnung der transformationsmatrix in scene implementiert

--- framework/scene.hpp
+++ framework/scene.hpp
@@ -31,6 +31,41 @@ struct Scene {
 };
 
 Scene loadSDF(std::string const& filename);
+glm::mat4 transform(float s1,s2,s3,rw,r1,r2,r3,t1,t2,t3)
+{
+  glm::mat4 trans(1.0f); 
+  trans[3] = glm::vec4(glm::vec3(t1,t2,t3),1.0f);
+  //auto erg = trans * vec;
+  glm::mat4 scale(1.0f);
+  scale[0][0] = s1;
+  scale[1][1] = s2;
+  scale[2][2] = s3;
+  glm::mat4 rotate (1.0f);
+  if(r1 == 1)
+  {
+    rotate[1][1] = cos(rw);
+    rotate[2][1] = -sin(rw);
+    rotate[1][2] = sin(rw);
+    rotate[2][2] = cos(rw);
+  }
+  if(r2== 1)
+  {
+    rotate[0][0] = cos(rw);
+    rotate[0][2] = -sin(rw);
+    rotate[2][0] = sin(rw);
+    rotate[2][2] = cos(rw);
+  }
+  if(r3 == 1)
+  {
+    rotate[0][0] = cos(rw);
+    rotate[1][0] = -sin(rw);
+    rotate[0][1] = sin(rw);
+    rotate[1][1] = cos(rw);
+  }
+  //erg = rotate * erg;
+  //erg = scale * erg;
+  return trans*rotate*scale;
+}
 
 
 #endif 
